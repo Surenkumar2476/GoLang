99487444a087416638d8fe0af591f9d5701a2e7e
Surenkumar2476 <surenkumar2476@gmail.com>
2024-06-03 20:26:49 +0530

    variables hands on

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const LoginToken string = "ajdowjfownfowef" //public - a variable start with capital letter it is a public varibale LoginToken
+
 func main() {
 
 	// string variable
@@ -12,4 +14,35 @@ func main() {
 	var islocatefile bool = true
 	fmt.Println(islocatefile)
 	fmt.Printf("Variable type is : %T \n", islocatefile)
+
+	var smallvalue uint8 = 255
+	fmt.Println(smallvalue)
+	fmt.Printf("Variable type is : %T \n", smallvalue)
+
+	var smallfloat float32 = 255.34533443434
+	fmt.Println(smallfloat)
+	fmt.Printf("Variable type is : %T \n", smallfloat)
+
+	// default values and some aliases
+
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable type is : %T \n", anotherVariable)
+
+	var anotherString string
+	fmt.Println(anotherString)
+	fmt.Printf("Variable type is : %T \n", anotherString)
+
+	// implicit types
+
+	var websites = "www.google.com"
+	fmt.Println(websites)
+
+	// no var style  // := is the walrus operator    and   walrus is not working in global variable declaration
+
+	numberOfUser := 8978675
+	fmt.Println(numberOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable type is : %T \n", LoginToken)
 }
