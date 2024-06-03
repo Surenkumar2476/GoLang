bc2b3aa7f6622040e5ab0479687a81472d8991f4
Surenkumar2476 <surenkumar2476@gmail.com>
2024-06-03 19:52:18 +0530

    variables and helloworld initial commit

---
+++ 02variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// string variable
+	var username string = "Surenkumar"
+	fmt.Println(username)
+	fmt.Printf("Variable type is : %T", username)
+}
+ 
